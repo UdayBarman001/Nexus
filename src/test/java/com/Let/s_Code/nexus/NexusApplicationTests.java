508b059514dc99a2f1872073654e1415e5e6d396
Uday Barman <udaybarman1321@gmail.com>
2026-05-21 15:00:08 +0530

    chore: secure initial setup with comprehensive gitignore

---
+++ src/test/java/com/Let/s_Code/nexus/NexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Let.s_Code.nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
